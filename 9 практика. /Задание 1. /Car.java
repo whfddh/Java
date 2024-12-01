14aabe65f40040746fc0165e5cd179eead9a7c65
whfddh <146356000+whfddh@users.noreply.github.com>
2024-12-01 17:05:22 +0300

    Create Car.java

---
+++ 9 практика. /Задание 1. /Car.java
@@ -0,0 +1,9 @@
+public class Car implements Nameable{
+    private String name;
+    public String getName(){
+       return name;
+    }
+    public Car(String name){
+        this.name = name;
+    }
+}
