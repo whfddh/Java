4c5cf214534c3cd84b08b2313089e7b6c792c300
whfddh <146356000+whfddh@users.noreply.github.com>
2024-12-01 18:46:35 +0300

    Update FileReplace.java

--- 13 практика. /FileReplace.java
+++ 13 практика. /FileReplace.java
@@ -1,1 +1,33 @@
+import java.io.*;
+import java.util.Scanner;
 
+public class FileReplace{
+    public static void main(String[] args){
+        try(FileReader reader = new FileReader("C:\\Users\\malol\\OneDrive\\Рабочий стол\\Новая папка (2)\\123.txt")){
+            int c;
+            while((c = reader.read()) != -1){
+                System.out.print((char) c);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        try (FileWriter writer = new FileWriter("C:\\Users\\malol\\OneDrive\\Рабочий стол\\Новая папка (2)\\123.txt  ")){
+            writer.write(text);
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        scanner.close();
+        try(FileReader reader = new FileReader("C:\\Users\\malol\\OneDrive\\Рабочий стол\\Новая папка (2)\\123.txt")){
+            int c;
+            while((c = reader.read()) != -1){
+                System.out.print((char) c);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
