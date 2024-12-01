d4555f078621cd3f1b23ef4ea059b77925074e8e
whfddh <146356000+whfddh@users.noreply.github.com>
2024-12-01 19:00:10 +0300

    Create StudentView.java

---
+++ 15 практика. /StudentView.java
@@ -0,0 +1,7 @@
+public class StudentView {
+    public void printStudentDetails(String studentName, String studentRollNo){
+        System.out.println("Student: ");
+        System.out.println("Name: "   + studentName);
+        System.out.println("RollNo: " + studentRollNo);
+    }
+}
