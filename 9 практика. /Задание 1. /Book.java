23be6639ad61a6fcda125b2cecf2d3ecc49ac383
whfddh <146356000+whfddh@users.noreply.github.com>
2024-12-01 17:05:06 +0300

    Create Book.java

---
+++ 9 практика. /Задание 1. /Book.java
@@ -0,0 +1,9 @@
+public class Book implements Nameable{
+    private String name;
+    public String getName(){
+        return name;
+    }
+    public Book(String name){
+        this.name = name;
+    }
+}
