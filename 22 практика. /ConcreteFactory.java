51b969954b3f4442bd6688191400a21205384306
whfddh <146356000+whfddh@users.noreply.github.com>
2024-12-02 15:19:59 +0300

    Create ConcreteFactory.java

---
+++ 22 практика. /ConcreteFactory.java
@@ -0,0 +1,8 @@
+public class ConcreteFactory implements ComplexAbstractFactory {
+    public Complex createComplex(){
+        return new Complex(0, 0);
+    };
+    public Complex CreateComplex(int real, int image){
+        return new Complex(real, image);
+    };
+}
