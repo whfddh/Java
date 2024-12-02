3473cc484b08bdcde847f51481db68aa666513b3
whfddh <146356000+whfddh@users.noreply.github.com>
2024-12-02 16:37:50 +0300

    Create MovableCircle.java

---
+++ 5 практика./4-5 задания. /MovableCircle.java
@@ -0,0 +1,31 @@
+public class MovableCircle implements Movable {
+    private int radius;
+    private MovablePoint center;
+
+    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius){
+        this.radius = radius;
+        this.center = new MovablePoint(x, y, xSpeed, ySpeed);
+    }
+
+    @Override
+    public void moveUp(){
+        center.moveUp();
+    }
+    @Override
+    public void moveRight(){
+        center.moveRight();
+    }
+    @Override
+    public void moveLeft(){
+        center.moveLeft();
+    }
+    @Override
+    public void moveDown(){
+        center.moveDown();
+    }
+
+    @Override
+    public String toString() {
+        return "MovableCircle(" + "center = " + center.x + ", " + center.y + ", radius = " + radius + ')';
+    }
+}
