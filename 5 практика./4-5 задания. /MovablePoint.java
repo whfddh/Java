550284ddd5e0b26847decf95b19b141a8a4ed8fd
whfddh <146356000+whfddh@users.noreply.github.com>
2024-12-02 16:35:53 +0300

    Update MovablePoint.java

--- 5 практика./4-5 задания. /MovablePoint.java
+++ 5 практика./4-5 задания. /MovablePoint.java
@@ -1,40 +1,48 @@
-public class MovablePoint implements Movable {
-    int x, y, xSpeed, ySpeed;
+  public class MovablePoint implements Movable{
+    protected int x;
+    protected int y;
+    protected int ySpeed;
+    protected int xSpeed;
 
-    public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+    public MovablePoint(int x, int y, int xSpeed, int ySpeed){
         this.x = x;
         this.y = y;
-        this.xSpeed = xSpeed;
         this.ySpeed = ySpeed;
+        this.xSpeed = xSpeed;
+    }
+    public int getX() {
+        return x;
     }
 
+    public int getY() {
+        return y;
+    }
+
+    public int getXSpeed() {
+        return xSpeed;
+    }
+
+    public int getYSpeed() {
+        return ySpeed;
+    }
     @Override
     public String toString() {
-        return "MovablePoint{" +
-                "x=" + x +
-                ", y=" + y +
-                ", xSpeed=" + xSpeed +
-                ", ySpeed=" + ySpeed +
-                '}';
+        return "MovablePoint: Point (" + x + ", " + y + ")";
     }
-
     @Override
-    public void moveUp() {
-        y += ySpeed;
+    public void moveUp(){
+        y++;
     }
-
     @Override
-    public void moveDown() {
-        y -= ySpeed;
+    public void moveDown(){
+        y--;
     }
-
     @Override
-    public void moveLeft() {
-        x -= xSpeed;
+    public void moveLeft(){
+        x--;
     }
-
     @Override
-    public void moveRight() {
-        x += xSpeed;
+    public void moveRight(){
+        x++;
     }
 }
