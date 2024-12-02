3044d674340940e4419c5d29014bbd9573ea5a3d
whfddh <146356000+whfddh@users.noreply.github.com>
2024-12-02 16:37:25 +0300

    Create Ball.java

---
+++ 5 практика./4-5 задания. /Ball.java
@@ -0,0 +1,34 @@
+public class Ball {
+    private double x = 0.0;
+    private double y = 0.0;
+    public Ball(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    public Ball(){
+        this(0,0);
+    }
+    public double getX(){
+        return x;
+    }
+    public void setX(double x){
+        this.x = x;
+    }
+    public double getY(){
+        return y;
+    }
+    public void setY(double y){
+        this.y = y;
+    }
+    public void setXY(double x, double y){
+        this.x = x;
+        this.y =y;
+    }
+    public void move(double xDisp, double yDisp){
+        x = x + xDisp;
+        y = y + yDisp;
+    }
+    public String toString(){
+        return "Ball @ (" + x + ", " + y + ")";
+    }
+}
