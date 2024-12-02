e81566a9f04b2a0d533a231a693d41306cfc4408
whfddh <146356000+whfddh@users.noreply.github.com>
2024-12-02 15:37:23 +0300

    Create OrdersManager.java

---
+++ 24 практика. /OrdersManager.java
@@ -0,0 +1,7 @@
+public interface OrdersManager {
+    int itemsQuantity(String itemName);
+    int itemsQuantity(MenuItem item);
+    Order[] getOrders();
+    int ordersCostSummary();
+    int ordersQuantity();
+}
