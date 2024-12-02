1b56f1b458c72e899f10c42c0a86c568d7b72911
whfddh <146356000+whfddh@users.noreply.github.com>
2024-12-02 14:54:29 +0300

    Create Animal.java

---
+++ 18 практика. /Animal.java
@@ -0,0 +1,33 @@
+import java.io.Serializable;
+
+public class Animal implements Serializable {
+    public String name;
+    public double size;
+    public Animal(String name, double size){
+        this.name = name;
+        this.size = size;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", size=" + size +
+                '}';
+    }
+}
