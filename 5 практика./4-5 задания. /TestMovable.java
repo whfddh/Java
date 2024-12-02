70e3d6ed134834574c15ee468a234bc298aedf44
whfddh <146356000+whfddh@users.noreply.github.com>
2024-12-02 16:36:55 +0300

    Update TestMovable.java

--- 5 практика./4-5 задания. /TestMovable.java
+++ 5 практика./4-5 задания. /TestMovable.java
@@ -1,25 +1,32 @@
 public class TestMovable {
-    public static void main(String[] args) {
-        Movable m = new MovablePoint(0, 0, 1, 4);
-        System.out.println("Point:\n");
-        System.out.println(m);
-        m.moveUp();
-        m.moveRight();
-        System.out.println("Up+Right -> " + m);
-
-        System.out.println("\nRectangle:\n");
-        m = new MovableRectangle(0, 0, 4, 4, 2,4);
-        System.out.println(m);
-        m.moveRight();
-        m.moveDown();
-        System.out.println("Right+Down -> " + m);
-
-        System.out.println("\nChanging speed for rectangle:");
-        MovableRectangle r = (MovableRectangle) m;
-        try{
-            r.moveDown();
-        }catch (IllegalStateException e){
-            System.out.println(e);
-        }
+    public static void main(String[] args){
+    MovablePoint point = new MovablePoint(0, 0, 0, 0);
+    System.out.println("Movable Point info: ");
+    System.out.println(point);
+    point.moveDown();
+    System.out.println("After MoveDown: ");
+    System.out.println(point);
+    point.moveLeft();
+    System.out.println("After moveLeft:");
+    System.out.println(point);
+    MovableCircle circle = new MovableCircle(5, 6, 7, 8, 9);
+    System.out.println("MovableCircle:");
+    System.out.println(circle);
+    circle.moveDown();
+    System.out.println("After moveDown:");
+    System.out.println(circle);
+    circle.moveRight();
+    System.out.println("After moveRight:");
+    System.out.println(circle);
+    MovableRectangle rectangle = new MovableRectangle(0, 0, 5, 4, 1, 1);
+    System.out.println("MovableRectangle:");
+    System.out.println(rectangle);
+    rectangle.moveUp();
+    System.out.println("After moveUp:");
+    System.out.println(rectangle);
+    rectangle.moveLeft();
+    System.out.println("After moveLeft:");
+    System.out.println(rectangle);
+    System.out.println("\nChecking same speed: " + rectangle.SameSpeed());
     }
 }
